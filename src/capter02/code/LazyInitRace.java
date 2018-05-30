6c3b64d45dbe61a3f08c8548300097657a4f97ed
snail <xuchuanliang361@163.com>
2018-05-30 08:34:51 +0800

    提交代码

---
+++ src/capter02/code/LazyInitRace.java
@@ -0,0 +1,15 @@
+package capter02.code;
+
+public class LazyInitRace {
+
+    private LazyInitRace lazyInitRace = null;
+
+    public synchronized   LazyInitRace getInstatnce(){
+        if(null==lazyInitRace){
+            lazyInitRace = new LazyInitRace();
+        }
+        return lazyInitRace;
+    }
+
+    private LazyInitRace(){}
+}
---
+++ src/capter03/code/ThreadLoadHolder.java
@@ -0,0 +1,9 @@
+package capter03.code;
+
+public class ThreadLoadHolder {
+    private static ThreadLocal<Integer> integerThreadLocal = new ThreadLocal<>();
+    public void test(){
+        integerThreadLocal.set(12);
+        integerThreadLocal.get();
+    }
+}
