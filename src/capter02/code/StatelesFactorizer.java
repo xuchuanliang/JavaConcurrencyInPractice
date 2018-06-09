bde285247d0331b71aa1763b31571e7509b6c52d
snail <xuchuanliang361@163.com>
2018-06-09 18:21:03 +0800

    提交capter04

--- src/capter02/code/StatelesFactorizer.java
+++ src/capter02/code/StatelesFactorizer.java
@@ -1,34 +1,34 @@
-package capter02.code;
-
-import javax.servlet.*;
-import java.io.IOException;
-import java.math.BigInteger;
-import java.util.concurrent.atomic.AtomicReference;
-
-public class StatelesFactorizer implements Servlet {
-    @Override
-    public void init(ServletConfig servletConfig) throws ServletException {
-
-    }
-
-    @Override
-    public ServletConfig getServletConfig() {
-        return null;
-    }
-
-    private long count = 0;
-
-    @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-    }
-
-    @Override
-    public String getServletInfo() {
-        return null;
-    }
-
-    @Override
-    public void destroy() {
-
-    }
-}
+//package capter02.code;
+//
+//import javax.servlet.*;
+//import java.io.IOException;
+//import java.math.BigInteger;
+//import java.util.concurrent.atomic.AtomicReference;
+//
+//public class StatelesFactorizer implements Servlet {
+//    @Override
+//    public void init(ServletConfig servletConfig) throws ServletException {
+//
+//    }
+//
+//    @Override
+//    public ServletConfig getServletConfig() {
+//        return null;
+//    }
+//
+//    private long count = 0;
+//
+//    @Override
+//    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+//    }
+//
+//    @Override
+//    public String getServletInfo() {
+//        return null;
+//    }
+//
+//    @Override
+//    public void destroy() {
+//
+//    }
+//}
