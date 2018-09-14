31c86d797eace9faab9894a4f68aadeed5b54034
snail <xuchuanliang361@163.com>
2018-09-14 08:36:20 +0800

    十一章：锁分解

---
+++ src/capter11/com/snail/b1/AttributeStore.java
@@ -0,0 +1,22 @@
+package capter11.com.snail.b1;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+/**
+ * 将一个锁不必要的持有过长时间
+ */
+public class AttributeStore {
+    private final Map<String,String> attribute = new HashMap<>();
+
+    public synchronized boolean userLocationMatches(String name,String regexp){
+        String key = "user."+name+".location";
+        String location = attribute.get(key);
+        if(location == null){
+            return false;
+        }else{
+            return Pattern.matches(regexp,location);
+        }
+    }
+}
---
+++ src/capter11/com/snail/b2/ServerStatus.java
@@ -0,0 +1,20 @@
+package capter11.com.snail.b2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 该demo多个方法共用同一个对象锁，保护了不同的状态变量，导致对锁的竞争程度较大
+ * 保护两个不同的状态变量 users和queries 此处使用同一个对象锁，会提高锁的竞争性，建议降低锁的粒度
+ */
+public class ServerStatus {
+
+    private final Set<String> users = new HashSet<>();
+    private final Set<String> queries = new HashSet<>();
+
+    public synchronized void addUser(String u){users.add(u);}
+    public synchronized void removeUser(String u){users.remove(u);}
+
+    public synchronized void addQueries(String q){queries.add(q);}
+    public synchronized void removeQueries(String q){queries.remove(q);}
+}
