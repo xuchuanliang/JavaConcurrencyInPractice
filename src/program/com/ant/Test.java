49c2a571a19e5b251ac9e7066fd7d09e8fb36a6c
xuchuanliang <xuchuanliang361@163.com>
2019-08-29 12:23:29 +0800

    commit

---
+++ src/program/com/ant/Test.java
@@ -0,0 +1,21 @@
+package program.com.ant;
+
+/**
+ * @author xuchuanliangbt
+ * @title: Test
+ * @projectName JavaConcurrencyInPractice
+ * @description:
+ * @date 2019/8/2717:04
+ * @Version
+ */
+public class Test {
+    public static void main(String[] args){
+
+    }
+
+    public static void test1(){
+        short a = 1;
+        int b = 2;
+        long c = 3;
+    }
+}
